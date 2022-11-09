09f8510779c7206b7adb184e0ceea5feaf5876f6
numanhan <numanhanduran@MacBook-Pro-von-Numanhan.local>
2022-11-09 16:00:30 +0100

    added some functions

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 5000,
   e2e: {
+    defaultCommandTimeout:6000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    
   },
+  watchForFileChanges:false,
+  
 });
