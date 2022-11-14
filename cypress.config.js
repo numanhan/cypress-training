8eec40f1e0aef9868b4ab4d5c6970481e97cecc1
numanhan <numanhanduran@MacBook-Pro-von-Numanhan.local>
2022-11-14 16:09:07 +0100

    Filtering tasks

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '9kvcgk',
   defaultCommandTimeout: 5000,
   e2e: {
-    defaultCommandTimeout:6000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/1-getting-started/todomvc-filtering.cy.js
+++ cypress/e2e/1-getting-started/todomvc-filtering.cy.js
@@ -1,2 +1,31 @@
 /// <reference types="cypress"/>
 
+describe('Filtering Process', () => {
+    beforeEach(() => {
+        cy.visit("http://todomvc-app-for-testing.surge.sh/");
+
+        cy.get('.new-todo').type('Clean the room {enter}')
+        cy.get('.new-todo').type('Learn Cypress {enter}')
+        cy.get('.new-todo').type('Make Plan weekly{enter}')
+
+        cy.get('.todo-list li:nth-child(2) .toggle').click()
+    })
+
+    it('Should filter "Active" tasks', ()=>{
+        cy.contains('Active').click()
+
+        cy.get('.todo-list li').should('have.length', 2)
+    })
+
+    it('Should filter "Completed" tasks', ()=>{
+        cy.contains('Completed').click()
+
+        cy.get('.todo-list li').should('have.length', 1)
+    })
+
+    it('Should filter "All" tasks', ()=>{
+        cy.contains('All').click()
+
+        cy.get('.todo-list li').should('have.length', 3)
+    })
+})
